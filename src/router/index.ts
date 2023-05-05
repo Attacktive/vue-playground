a3cc5ac439f005b8469b281c55d3c99c957a590c
Attacktive <tribute2pantera@gmail.com>
2023-05-06 00:19:28 +0900

    feature: implement page 'Repositories'

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,6 +17,11 @@ const router = createRouter({
 			// which is lazy-loaded when the route is visited.
 			component: () => import("../views/AboutView.vue"),
 		},
+		{
+			path: "/repositories",
+			name: "repositories",
+			component: () => import("../views/RepositoriesView.vue"),
+		},
 	],
 });
 
