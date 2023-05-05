e2d511528883f123d11137b2f79ba4c7935bd09a
Attacktive <tribute2pantera@gmail.com>
2023-05-06 01:18:13 +0900

    chore: reformat entire codebase
    Tabs FTW 🤘

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,7 +7,7 @@ const router = createRouter({
 		{
 			path: "/",
 			name: "home",
-			component: HomeView,
+			component: HomeView
 		},
 		{
 			path: "/about",
@@ -15,14 +15,14 @@ const router = createRouter({
 			// route level code-splitting
 			// this generates a separate chunk (About.[hash].js) for this route
 			// which is lazy-loaded when the route is visited.
-			component: () => import("../views/AboutView.vue"),
+			component: () => import("../views/AboutView.vue")
 		},
 		{
 			path: "/repositories",
 			name: "repositories",
-			component: () => import("../views/RepositoriesView.vue"),
-		},
-	],
+			component: () => import("../views/RepositoriesView.vue")
+		}
+	]
 });
 
 export default router;
--- src/views/RepositoriesView.vue
+++ src/views/RepositoriesView.vue
@@ -3,30 +3,38 @@
 		<h1>The repositories I've been working on.</h1>
 		<ul>
 			<li>
-				<a href="https://github.com/Attacktive/vue-exercise" target="_blank">vue-exercise</a>: This very repository!
+				<a href="https://github.com/Attacktive/vue-exercise" target="_blank">vue-exercise</a>:
+				This very repository!
 			</li>
 			<li>
-				troubleshooter-editor: The editor of the game <a href="https://store.steampowered.com/app/470310/TROUBLESHOOTER_Abandoned_Children">TROUBLESHOOTER: Abandoned Children</a>.
+				troubleshooter-editor: The editor of the game
+				<a href="https://store.steampowered.com/app/470310/TROUBLESHOOTER_Abandoned_Children">TROUBLESHOOTER: Abandoned Children</a>.
 				<ul>
 					<li>
-						<a href="https://github.com/Attacktive/troubleshooter-editor-back-end">troubleshooter-editor-back-end</a>: The backend written in <em>Kotlin</em>
+						<a href="https://github.com/Attacktive/troubleshooter-editor-back-end">troubleshooter-editor-back-end</a>:
+						The backend written in <em>Kotlin</em>
 					</li>
 					<li>
-						<a href="https://github.com/Attacktive/troubleshooter-editor-front-end">troubleshooter-editor-front-end</a>: The frontend with <em>React</em> which was archived ☠
+						<a href="https://github.com/Attacktive/troubleshooter-editor-front-end">troubleshooter-editor-front-end</a>:
+						The frontend with <em>React</em> which was archived ☠
 					</li>
 					<li>
-						<a href="https://github.com/Attacktive/troubleshooter-editor-front-end-vue">troubleshooter-editor-front-end-vue</a>: The frontend with <em>Vue.js</em>, <em>TypeScript</em>
+						<a href="https://github.com/Attacktive/troubleshooter-editor-front-end-vue">troubleshooter-editor-front-end-vue</a>:
+						The frontend with <em>Vue.js</em>, <em>TypeScript</em>
 					</li>
 				</ul>
 			</li>
 			<li>
-				<a href="https://github.com/Attacktive/jandi-shortcut-disabler" target="_blank">jandi-shortcut-disabler</a>: A Chrome extension to disable annoying keyboard shortcuts on <a href="https://www.jandi.com">Jandi.com</a>
+				<a href="https://github.com/Attacktive/jandi-shortcut-disabler" target="_blank">jandi-shortcut-disabler</a>:
+				A Chrome extension to disable annoying keyboard shortcuts on <a href="https://www.jandi.com">Jandi.com</a>
 			</li>
 			<li>
-				<a href="https://github.com/Attacktive/javascript-loosely-equal-example-giver" target="_blank">javascript-loosely-equal-example-giver</a>: A small research on <em>JavaScript</em> loose equality operator (<code>==</code>)
+				<a href="https://github.com/Attacktive/javascript-loosely-equal-example-giver" target="_blank">javascript-loosely-equal-example-giver</a>:
+				A small research on <em>JavaScript</em> loose equality operator (<code>==</code>)
 			</li>
 			<li>
-				<a href="https://github.com/Attacktive/vpn-manager" target="_blank">vpn-manager</a>: A <em>Java</em> application which periodically checks on the internet connection and turn on the <a href="https://mudfish.net">Mudfish VPN</a> item
+				<a href="https://github.com/Attacktive/vpn-manager" target="_blank">vpn-manager</a>:
+				A <em>Java</em> application which periodically checks on the internet connection and turn on the <a href="https://mudfish.net">Mudfish VPN</a> item
 			</li>
 			<li>
 				...and more on <a href="https://github.com/Attacktive?tab=repositories">my repositories</a>!
