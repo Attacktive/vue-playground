bdc27b691538bc78a7847cdbaaab92a3a4d638bd
Attacktive <tribute2pantera@gmail.com>
2024-02-27 20:49:07 +0900

    feature: add and remove repository entries

--- src/views/RepositoriesView.vue
+++ src/views/RepositoriesView.vue
@@ -12,38 +12,35 @@
 				<ul>
 					<li>
 						<a href="https://github.com/Attacktive/troubleshooter-editor-back-end">troubleshooter-editor-back-end</a>:
-						The backend written in <em>Kotlin</em>
+						The backend written in <em>Kotlin</em>.
 					</li>
 					<li>
 						<a href="https://github.com/Attacktive/troubleshooter-editor-front-end-svelte">troubleshooter-editor-front-end-svelte</a>:
-						The frontend with <em>Svelte</em>, <em>TypeScript</em>, and <em>Tailwind CSS</em>
-					</li>
-					<li>
-						<a href="https://github.com/Attacktive/troubleshooter-editor-front-end">troubleshooter-editor-front-end</a>:
-						The frontend with <em>React</em>, and <em>Bootstrap</em> which is archived ☠
-					</li>
-					<li>
-						<a href="https://github.com/Attacktive/troubleshooter-editor-front-end-vue">troubleshooter-editor-front-end-vue</a>:
-						The frontend with <em>Vue.js</em>, <em>TypeScript</em>, and <em>Bootstrap</em> which is archived ☠
+						The frontend with <em>Svelte</em>, <em>TypeScript</em>, and <em>Tailwind CSS</em>.
 					</li>
 				</ul>
 			</li>
 			<li>
+				<a href="https://github.com/Attacktive/witchspring-r-editor">witchspring-r-editor</a>:
+				The web-based save file editor for the game <a href="https://store.steampowered.com/app/1958220/WitchSpring_R/">WitchSpring R</a>
+				powered by <em>Svelte</em>, <em>TypeScript</em>, and <em>Tailwind CSS</em>.
+			</li>
+			<li>
 				<a href="https://github.com/Attacktive/jandi-shortcut-disabler" target="_blank">jandi-shortcut-disabler</a>:
-				A Chrome extension to disable annoying keyboard shortcuts on <a href="https://www.jandi.com">Jandi.com</a>
+				A Chrome extension to disable annoying keyboard shortcuts on <a href="https://www.jandi.com">Jandi.com</a>.
 			</li>
 			<li>
 				<a href="https://github.com/Attacktive/javascript-loosely-equal-example-giver" target="_blank">javascript-loosely-equal-example-giver</a>:
-				A small research on <em>JavaScript</em> loose equality operator (<code>==</code>)
+				A small research on <em>JavaScript</em> loose equality operator (<code>==</code>).
 			</li>
 			<li>
 				<a href="https://github.com/Attacktive/vpn-manager" target="_blank">vpn-manager</a>:
-				A <em>Java</em> application which periodically checks on the internet connection and turn on the <a href="https://mudfish.net">Mudfish VPN</a> item
+				A <em>Java</em> application which periodically checks on the internet connection and turn on the <a href="https://mudfish.net">Mudfish VPN</a> item.
 			</li>
 			<li>
 				<a href="https://github.com/Attacktive/ipa-to-pronunciation-respelling" target="_blank">ipa-to-pronunciation-respelling</a>:
 				A <em>Bootstrap</em>-powered HTML page that converts "cryptic" <a href="https://en.wikipedia.org/wiki/International_Phonetic_Alphabet">IPA</a> expressions
-				into <a href="https://en.wikipedia.org/wiki/Pronunciation_respelling_for_English">English pronunciation respelling</a>
+				into <a href="https://en.wikipedia.org/wiki/Pronunciation_respelling_for_English">English pronunciation respelling</a>.
 			</li>
 			<li>
 				...and more on <a href="https://github.com/Attacktive?tab=repositories">my repositories</a>!
