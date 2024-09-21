c000a17f32c32c0d167b85c9ee32f88d17039c86
Attacktive <tribute2pantera@gmail.com>
2024-09-21 18:06:11 +0900

    refactor: separate single gigantic RepositoriesView.vue into view and components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { computed, ref } from "vue";
 import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from "@/components/hello-world/HelloWorld.vue";
 
 const subject = computed(() => {
 	if (secondsMod10.value < 5) {
