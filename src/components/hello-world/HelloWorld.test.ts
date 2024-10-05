0af6f1c95014dbe18f21d5972785256ba06ac22b
Attacktive <tribute2pantera@gmail.com>
2024-10-05 14:27:02 +0900

    refactor: reformat preexisting test code
    https://github.com/Attacktive/vue-exercise/issues/511

--- src/components/hello-world/HelloWorld.test.ts
+++ src/components/hello-world/HelloWorld.test.ts
@@ -1,11 +1,14 @@
 import { describe, expect, it } from "vitest";
-
 import { mount } from "@vue/test-utils";
 import HelloWorld from "@/components/hello-world/HelloWorld.vue";
 
-describe("HelloWorld", () => {
-	it("renders properly", () => {
-		const wrapper = mount(HelloWorld, { props: { message: "Hello Vitest" } });
-		expect(wrapper.text()).toContain("Hello Vitest");
+describe(
+	"HelloWorld",
+	() => {
+		it(
+			"renders properly",
+			() => {
+				const wrapper = mount(HelloWorld, { props: { message: "Hello Vitest" } });
+				expect(wrapper.text()).toContain("Hello Vitest");
+			});
 	});
-});
