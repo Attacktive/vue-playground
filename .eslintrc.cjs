d6f43be7afee2c5973b8d3c3546f33905f358c6d
Attacktive <tribute2pantera@gmail.com>
2024-10-05 14:34:43 +0900

    refactor: make ESLint accept unused variable whose name starts with underscore
    https://github.com/Attacktive/vue-exercise/issues/511
    https://github.com/Attacktive/vue-exercise/pull/512/files#annotation_27074456475

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,5 +10,15 @@ module.exports = {
 	],
 	parserOptions: {
 		ecmaVersion: "latest"
+	},
+	rules: {
+		"@typescript-eslint/no-unused-vars": [
+			"warn",
+			{
+				"argsIgnorePattern": "^_",
+				"varsIgnorePattern": "^_",
+				"caughtErrorsIgnorePattern": "^_"
+			}
+		]
 	}
 };
